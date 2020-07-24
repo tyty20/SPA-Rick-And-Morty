311c8bcbc82aa662078a5f102c59fc606f67248c
Julian Carelli <julianignaciocarelli@gmail.com>
2020-07-24 18:15:04 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules:[
+            {
+                test: /\.scss$/,
+                use:['style-loader', 'css-loader', 'sass-loader']
+            },
+
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    
+    plugins: [
+        new HtmlWebPackPlugin(
+            {
+                inject:true,
+                template:'./public/index.html',
+                filename:'./index.html'
+                
+            }
+        )
+    ]
+}
