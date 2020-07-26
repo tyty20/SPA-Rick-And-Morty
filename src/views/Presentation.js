2f0f2d381597f09fdfea7d688abce7fa7e6187d6
Julian Carelli <julianignaciocarelli@gmail.com>
2020-07-26 17:57:47 -0300

    Add new files and new styles with sass

---
+++ src/views/Presentation.js
@@ -0,0 +1,18 @@
+const Presentation = () => {
+    const view = `
+    <div class="content__presentation">
+        <div class="content__presentation--img">
+            <img src="https://images6.alphacoders.com/909/thumb-1920-909641.png" alt="rickAndMorty">
+        </div>
+        <div class="content__presentation--text">
+            <h2>Pagina construida gracias a la Api de Rick And Morty</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni possimus voluptatem libero illo facere recusandae, id nostrum quo accusantium nesciunt vitae accusamus numquam rem tempore odit! Mollitia animi itaque rerum!
+            illo facere recusandae,
+            </p>
+        </div>
+    </div>
+    `
+    return view
+}
+
+export default Presentation
